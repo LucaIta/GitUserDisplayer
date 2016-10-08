e829bd1df240b14d7eb4273cbd5aaeeb3b396462
Luca Quatela <lucaqq@gmail.com>
2016-10-08 12:39:08 +0200

    removed key for calls

--- js/apiCall.js
+++ js/apiCall.js
@@ -1,11 +1,11 @@
-var apiKey = require('./.env').apiKey;
+// var apiKey = require('./.env').apiKey;
 var userRepositoriesArray = [];
 var Repository = require('../js/repository.js').Repository;
 
 exports.getAndDisplayRepos = function(userName, repositoryDisplayer){
   userRepositoriesArray = [];
   $('.output').empty();
-  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName).then(function(response){
 
     console.log(response);
     response.forEach(function(repository){
@@ -26,7 +26,7 @@ exports.getAndDisplayRepos = function(userName, repositoryDisplayer){
 
 exports.getAndDisplayUser = function(userName,userDisplayer){
 
-  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName).then(function(response){
     console.log(response);
     userDisplayer(response);
   });
